38ccf225e4b14f2d96d35b966ab18f3302025c3f
Utsa Ghosh <utsaghoshofficial@gmail.com>
2024-08-08 13:51:49 +0530

    Initial commit request sent done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+const WS_URL = "ws://localhost:8080";
 
 function App() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [receivedMessages, setReceivedMessages] = useState([]);
+
+  const { sendMessage, lastJsonMessage, readyState } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log("WebSocket connection established.");
+    },
+    onClose: () => {
+      console.log("WebSocket connection closed.");
+    },
+    share: true,
+    filter: () => false,
+    shouldReconnect: () => true,
+    protocols: "echo-protocol",
+  });
+
+  useEffect(() => {
+    if (lastJsonMessage !== null) {
+      console.log(JSON.stringify(lastJsonMessage.data));
+      setReceivedMessages([
+        ...receivedMessages,
+        JSON.stringify(lastJsonMessage.data),
+      ]);
+    }
+  }, [lastJsonMessage]);
+
+  useEffect(() => {
+    console.log("Connection state changed");
+    if (readyState === ReadyState.OPEN) {
+      sendMessage("connected");
+    }
+  }, [readyState]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h4>Messages:</h4>
+        <ul>
+          {receivedMessages.map((message, index) => {
+            <li key={index}>{message}</li>;
+          })}
+        </ul>
+        <div>
+          <br></br>
+          <input
+            type="text"
+            id="inputmsg"
+            placeholder="Enter your message"
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+          ></input>
+          <button
+            onClick={() => sendMessage(inputMessage)}
+            disabled={readyState !== ReadyState.OPEN}
+          >
+            Send
+          </button>
+        </div>
+        <br />
+        ReadyState: {readyState.toString()}
+        <br />
+      </div>
     </div>
   );
 }
