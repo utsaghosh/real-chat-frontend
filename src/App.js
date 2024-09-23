6510b36252bb282fcdcfda0c9f7d2053a189290f
Utsa Ghosh <utsaghoshofficial@gmail.com>
2024-09-24 01:57:48 +0530

    working

--- src/App.js
+++ src/App.js
@@ -4,33 +4,48 @@ import { useEffect, useState } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const WS_URL = "ws://localhost:8080";
+// const WS_URL = "wss://echo.websocket.org";
+// const WS_URL = "wss://websocket-echo.com/";
 
 function App() {
   const [inputMessage, setInputMessage] = useState("");
   const [receivedMessages, setReceivedMessages] = useState([]);
 
-  const { sendMessage, lastJsonMessage, readyState } = useWebSocket(WS_URL, {
-    onOpen: () => {
-      console.log("WebSocket connection established.");
-    },
-    onClose: () => {
-      console.log("WebSocket connection closed.");
-    },
-    share: true,
-    filter: () => false,
-    shouldReconnect: () => true,
-    protocols: "echo-protocol",
-  });
+  const { sendMessage, lastMessage, lastJsonMessage, readyState } =
+    useWebSocket(WS_URL, {
+      onOpen: () => {
+        console.log("WebSocket connection established.");
+      },
+      onClose: () => {
+        console.log("WebSocket connection closed.");
+      },
+      onMessage: (event) => {
+        console.log("Received message", event);
+      },
+      share: true,
+      shouldReconnect: () => true,
+      protocols: "echo-protocol",
+    });
+
+  // useEffect(() => {
+  //   if (lastJsonMessage !== null) {
+  //     console.log("lastJsonMessage: ", JSON.stringify(lastJsonMessage));
+  //     setReceivedMessages([
+  //       ...receivedMessages,
+  //       JSON.stringify(lastJsonMessage.data),
+  //     ]);
+  //   }
+  // }, [lastJsonMessage]);
 
   useEffect(() => {
-    if (lastJsonMessage !== null) {
-      console.log(JSON.stringify(lastJsonMessage.data));
+    if (lastMessage !== null) {
+      console.log("lastMessage: ", JSON.stringify(lastMessage));
       setReceivedMessages([
         ...receivedMessages,
-        JSON.stringify(lastJsonMessage.data),
+        JSON.stringify(lastMessage.data),
       ]);
     }
-  }, [lastJsonMessage]);
+  }, [lastMessage]);
 
   useEffect(() => {
     console.log("Connection state changed");
@@ -39,15 +54,30 @@ function App() {
     }
   }, [readyState]);
 
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
   return (
     <div className="App">
       <div>
-        <h4>Messages:</h4>
-        <ul>
-          {receivedMessages.map((message, index) => {
-            <li key={index}>{message}</li>;
-          })}
-        </ul>
+        <div>
+          <h4>Messages:</h4>
+          <ul>
+            {receivedMessages?.map((message, index) => (
+              <li key={index}>{message}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          {lastJsonMessage ? (
+            <span>Last message: {lastJsonMessage.data}</span>
+          ) : null}
+        </div>
         <div>
           <br></br>
           <input
@@ -65,7 +95,7 @@ function App() {
           </button>
         </div>
         <br />
-        ReadyState: {readyState.toString()}
+        ReadyState: {connectionStatus}
         <br />
       </div>
     </div>
